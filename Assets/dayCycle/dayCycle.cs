340bec13981202f2ac74fab4a8bd178214241135
nobidon <nobidon.tim@gmail.com>
2025-02-16 12:17:38 +0500

    newDayCycle

--- Assets/dayCycle/dayCycle.cs
+++ Assets/dayCycle/dayCycle.cs
@@ -6,6 +6,8 @@ public class DayNightCycle : MonoBehaviour
     public Light moon;
     public Material skyboxDay;
     public Material skyboxNight;
+    public GameObject sunObject;
+    public GameObject moonObject;
     public float dayDuration = 60f; // Длительность полного цикла дня (в секундах)
     public Gradient lightColor;
     public AnimationCurve lightIntensity;
@@ -22,6 +24,10 @@ public class DayNightCycle : MonoBehaviour
         float sunAngle = cycleProgress * 360f - 90f;
         sun.transform.rotation = Quaternion.Euler(sunAngle, 170f, 0f);
         moon.transform.rotation = Quaternion.Euler(sunAngle + 180f, 170f, 0f);
+        
+        // Перемещение солнца и луны в небе
+        sunObject.transform.position = sun.transform.forward * 100f;
+        moonObject.transform.position = moon.transform.forward * 100f;
         
         // Изменение цвета и интенсивности
         sun.color = lightColor.Evaluate(cycleProgress);
